708bbdc2881cfe9e243258502cca3169096ad87b
benjimania74 <lesdalton74@gmail.com>
2022-08-22 23:32:23 +0200

    commencement interface Android

--- Android/Android/MainActivity.cs
+++ Android/Android/MainActivity.cs
@@ -4,24 +4,21 @@ using Android.Runtime;
 using AndroidX.AppCompat.App;
 using Android.Widget;
 
-namespace Android
-{
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
-    {
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
+namespace Android{
+    [Activity(Label = "@string/app_name", Theme = "@style/Theme.MaterialComponents.Light.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity{
+        protected override void OnCreate(Bundle savedInstanceState){
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
 
-            TextView Tv = FindViewById<TextView>(Resource.Id.textView1);
-            Tv.Text = "Hello World !";
-            Tv.LineHeight = 50;
+            ImageView Iv = FindViewById<ImageView>(Resource.Id.add_element_button);
+            int Width = FindViewById<GridLayout>(Resource.Id.content_layout).Width - 320;
+            Iv.SetMaxWidth(Width);
+            Iv.SetMinimumWidth(Width);
         }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults){
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
--- Android/Android/obj/Debug/120/android/src/androidx/fragment/R.java
+++ Android/Android/obj/Debug/120/android/src/androidx/fragment/R.java
@@ -31,10 +31,10 @@ public final class R {
 		public static final int ttcIndex = 0x7f0301df;
 	}
 	public static final class color {
-		public static final int notification_action_color_filter = 0x7f050072;
-		public static final int notification_icon_bg_color = 0x7f050073;
-		public static final int ripple_material_light = 0x7f05007d;
-		public static final int secondary_text_default_material_light = 0x7f05007f;
+		public static final int notification_action_color_filter = 0x7f050073;
+		public static final int notification_icon_bg_color = 0x7f050074;
+		public static final int ripple_material_light = 0x7f05007e;
+		public static final int secondary_text_default_material_light = 0x7f050080;
 	}
 	public static final class dimen {
 		public static final int compat_button_inset_horizontal_material = 0x7f060053;
@@ -61,18 +61,18 @@ public final class R {
 		public static final int notification_top_pad_large_text = 0x7f0600d4;
 	}
 	public static final class drawable {
-		public static final int notification_action_background = 0x7f070072;
-		public static final int notification_bg = 0x7f070073;
-		public static final int notification_bg_low = 0x7f070074;
-		public static final int notification_bg_low_normal = 0x7f070075;
-		public static final int notification_bg_low_pressed = 0x7f070076;
-		public static final int notification_bg_normal = 0x7f070077;
-		public static final int notification_bg_normal_pressed = 0x7f070078;
-		public static final int notification_icon_background = 0x7f070079;
-		public static final int notification_template_icon_bg = 0x7f07007a;
-		public static final int notification_template_icon_low_bg = 0x7f07007b;
-		public static final int notification_tile_bg = 0x7f07007c;
-		public static final int notify_panel_notification_icon_bg = 0x7f07007d;
+		public static final int notification_action_background = 0x7f070074;
+		public static final int notification_bg = 0x7f070075;
+		public static final int notification_bg_low = 0x7f070076;
+		public static final int notification_bg_low_normal = 0x7f070077;
+		public static final int notification_bg_low_pressed = 0x7f070078;
+		public static final int notification_bg_normal = 0x7f070079;
+		public static final int notification_bg_normal_pressed = 0x7f07007a;
+		public static final int notification_icon_background = 0x7f07007b;
+		public static final int notification_template_icon_bg = 0x7f07007c;
+		public static final int notification_template_icon_low_bg = 0x7f07007d;
+		public static final int notification_tile_bg = 0x7f07007e;
+		public static final int notify_panel_notification_icon_bg = 0x7f07007f;
 	}
 	public static final class id {
 		public static final int accessibility_action_clickable_span = 0x7f080006;
@@ -113,37 +113,37 @@ public final class R {
 		public static final int action_image = 0x7f080031;
 		public static final int action_text = 0x7f080037;
 		public static final int actions = 0x7f080038;
-		public static final int async = 0x7f08003e;
-		public static final int blocking = 0x7f080041;
-		public static final int chronometer = 0x7f08004e;
-		public static final int dialog_button = 0x7f08005f;
-		public static final int forever = 0x7f08006e;
-		public static final int fragment_container_view_tag = 0x7f08006f;
-		public static final int icon = 0x7f080075;
-		public static final int icon_group = 0x7f080076;
-		public static final int info = 0x7f080079;
-		public static final int italic = 0x7f08007a;
-		public static final int line1 = 0x7f08007f;
-		public static final int line3 = 0x7f080080;
-		public static final int normal = 0x7f08008d;
-		public static final int notification_background = 0x7f08008e;
-		public static final int notification_main_column = 0x7f08008f;
-		public static final int notification_main_column_container = 0x7f080090;
-		public static final int right_icon = 0x7f08009c;
-		public static final int right_side = 0x7f08009d;
-		public static final int tag_accessibility_actions = 0x7f0800c5;
-		public static final int tag_accessibility_clickable_spans = 0x7f0800c6;
-		public static final int tag_accessibility_heading = 0x7f0800c7;
-		public static final int tag_accessibility_pane_title = 0x7f0800c8;
-		public static final int tag_screen_reader_focusable = 0x7f0800c9;
-		public static final int tag_transition_group = 0x7f0800ca;
-		public static final int tag_unhandled_key_event_manager = 0x7f0800cb;
-		public static final int tag_unhandled_key_listeners = 0x7f0800cc;
-		public static final int text = 0x7f0800cd;
-		public static final int text2 = 0x7f0800ce;
-		public static final int time = 0x7f0800d7;
-		public static final int title = 0x7f0800d8;
-		public static final int visible_removing_fragment_view_tag = 0x7f0800eb;
+		public static final int async = 0x7f08003f;
+		public static final int blocking = 0x7f080042;
+		public static final int chronometer = 0x7f08004f;
+		public static final int dialog_button = 0x7f080061;
+		public static final int forever = 0x7f08006f;
+		public static final int fragment_container_view_tag = 0x7f080070;
+		public static final int icon = 0x7f080076;
+		public static final int icon_group = 0x7f080077;
+		public static final int info = 0x7f08007a;
+		public static final int italic = 0x7f08007b;
+		public static final int line1 = 0x7f080080;
+		public static final int line3 = 0x7f080081;
+		public static final int normal = 0x7f08008e;
+		public static final int notification_background = 0x7f08008f;
+		public static final int notification_main_column = 0x7f080090;
+		public static final int notification_main_column_container = 0x7f080091;
+		public static final int right_icon = 0x7f08009e;
+		public static final int right_side = 0x7f08009f;
+		public static final int tag_accessibility_actions = 0x7f0800c7;
+		public static final int tag_accessibility_clickable_spans = 0x7f0800c8;
+		public static final int tag_accessibility_heading = 0x7f0800c9;
+		public static final int tag_accessibility_pane_title = 0x7f0800ca;
+		public static final int tag_screen_reader_focusable = 0x7f0800cb;
+		public static final int tag_transition_group = 0x7f0800cc;
+		public static final int tag_unhandled_key_event_manager = 0x7f0800cd;
+		public static final int tag_unhandled_key_listeners = 0x7f0800ce;
+		public static final int text = 0x7f0800cf;
+		public static final int text2 = 0x7f0800d0;
+		public static final int time = 0x7f0800d8;
+		public static final int title = 0x7f0800d9;
+		public static final int visible_removing_fragment_view_tag = 0x7f0800ec;
 	}
 	public static final class integer {
 		public static final int status_bar_notification_info_maxnum = 0x7f09000e;
@@ -158,7 +158,7 @@ public final class R {
 		public static final int notification_template_part_time = 0x7f0b0035;
 	}
 	public static final class string {
-		public static final int status_bar_notification_info_overflow = 0x7f0d002f;
+		public static final int status_bar_notification_info_overflow = 0x7f0d0030;
 	}
 	public static final class style {
 		public static final int TextAppearance_Compat_Notification = 0x7f0e0116;
